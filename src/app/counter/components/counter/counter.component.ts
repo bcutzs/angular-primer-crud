ba739c747f7db7dd725e15d352d8238883f83dfb
Brayan Cutz <brayancutz.96@gmail.com>
2023-05-20 12:43:53 -0600

    Fin de seccion 4

---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { AppComponent } from '../../../app.component';
+
+@Component({
+  selector: 'app-counter',
+  template: `
+  <h3>Counter: {{ counter }}</h3>
+
+  <button (click)="increaseBy(+1)">+1</button>
+  <button (click)="resetCounter()">Reset</button>
+  <button (click)="increaseBy(-1)">-1</button>
+  `
+})
+
+export class CounterComponent extends AppComponent{
+  public counter: number = 10;
+
+  increaseBy( value: number ):void {
+    this.counter += value;
+  }
+
+  resetCounter() {
+    this.counter = 10;
+  }
+}
